6b65a15868a199dbb9d3c5f7f0c7491dfe41f62d
Ram Goel <rgoel766@gmail.com>
2023-05-19 18:56:33 +0530

    added navbar

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './Home';
+import './App.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
